dd7e617d9b8319c69cc00cee2c86fb9729ec8ce1
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-12-09 00:08:27 +0300

    Use requests instead of urllib

--- utils/http.py
+++ utils/http.py
@@ -1,13 +1,8 @@
-import urllib
+import requests
 
 class HttpManager():
     """ Manages HTTP resquests """
 
     def make_request(self, url, data = None):
         """ Invokes an HTTP GET request """
-
-        req = urllib.urlopen(url, data)
-        ret = req.read()
-        req.close()
-
-        return ret
+        return requests.get(url).text
